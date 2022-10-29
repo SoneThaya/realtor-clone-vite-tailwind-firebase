7e3b5df719e8961c49a822c2e235dd125438989f
sone <sthaya@gmail.com>
2022-10-28 19:51:50 -0700

    worked on edit profile functionality and loading spinner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [pageState, setPageState] = useState("Sign in");
+
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign in");
+      }
+    });
+  }, []);
 
   const pathMatchRoute = (route: string) => {
     if (route === location.pathname) {
@@ -12,7 +26,7 @@ const Header = () => {
   };
 
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
@@ -42,11 +56,12 @@ const Header = () => {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/signin") && "text-black border-b-red-500"
+                (pathMatchRoute("/signin") || pathMatchRoute("/profile")) &&
+                "text-black border-b-red-500"
               }`}
-              onClick={() => navigate("/signin")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </div>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,14 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 
 const Profile = () => {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
 
   const [formData, setFormData] = useState({
     name: auth.currentUser?.displayName,
@@ -18,6 +22,31 @@ const Profile = () => {
     navigate("/");
   };
 
+  const handleNameChange = (e: any) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        const docRef = doc(db, "users", auth.currentUser?.uid);
+        await updateDoc(docRef, {
+          name: name,
+        });
+        toast.success("Profile details updated!");
+      }
+    } catch (error) {
+      toast.error("Could not update the profile details!");
+    }
+  };
+
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -28,8 +57,11 @@ const Profile = () => {
               type="text"
               id="name"
               value={name}
-              disabled
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6"
+              disabled={!changeDetail}
+              onChange={handleNameChange}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
             />
 
             <input
@@ -43,8 +75,14 @@ const Profile = () => {
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
               <p className="flex items-center">
                 Do you want to change your name?{" "}
-                <span className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1 cursor-pointer">
-                  Edit
+                <span
+                  onClick={() => {
+                    changeDetail && handleSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? "Apply Change" : "Edit"}
                 </span>
               </p>
               <p
